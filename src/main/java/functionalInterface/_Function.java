dc5adceb0015670bb4a0eb33f8aca880d16056ef
Dumilde Matos <dumildejosematos@gmail.com>
2021-10-12 04:28:23 +0100

    combine functions

--- src/main/java/functionalInterface/_Function.java
+++ src/main/java/functionalInterface/_Function.java
@@ -4,13 +4,16 @@ import java.util.function.Function;
 
 public class _Function {
     public static void main(String[] args) {
-        int increment = increment(0);
+        int increment = increment(1);
         System.out.println(increment);
-        int increment2 = incrementByOne.apply(0);
+        int increment2 = incrementByOneFunction.apply(1);
         System.out.println("With Functional: " + increment2);
+        Function<Integer, Integer> addByOneAndMultipyBy10 = incrementByOneFunction.andThen(multiplyBy10);
+        System.out.println(addByOneAndMultipyBy10.apply(1));
     }
 
-    static Function<Integer, Integer> incrementByOne = number -> number+1;
+    static Function<Integer, Integer> incrementByOneFunction = number -> number+1;
+    static Function<Integer, Integer> multiplyBy10 = number -> number * 10;
 
     static int increment (int number) {
         return number + 1;
