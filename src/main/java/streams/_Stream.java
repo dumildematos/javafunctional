c3ddc138ae0fe7537ef0d8fa96a3818cebb58e75
Dumilde Matos <dumildejosematos@gmail.com>
2021-10-16 21:20:26 +0100

    Streams

---
+++ src/main/java/streams/_Stream.java
@@ -0,0 +1,53 @@
+package streams;
+
+
+import imperative.Main;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static streams._Stream.Gender.FEMALE;
+import static streams._Stream.Gender.MALE;
+
+
+public class _Stream {
+    public static void main(String[] args) {
+
+        List<Person> people = List.of(
+                new Person("John", MALE),
+                new Person("Maria", FEMALE),
+                new Person("Aisha", FEMALE),
+                new Person("Alex", MALE),
+                new Person("Alice", FEMALE)
+        );
+
+        people.stream().map(person -> person.name)
+                .mapToInt(String::length)
+                .forEach(System.out::println);
+
+    }
+
+    static class Person {
+
+        private final String name;
+        private final Gender gender;
+
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
+}
